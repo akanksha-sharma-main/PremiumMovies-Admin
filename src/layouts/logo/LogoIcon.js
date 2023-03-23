69d7dc508226b573aef659fc4e04ad04a694e3b4
Aditya Sharma <akanksha.sharma.main@gmail.com>
2023-03-23 18:27:10 +0000

    Update LogoIcon.js

--- src/layouts/logo/LogoIcon.js
+++ src/layouts/logo/LogoIcon.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Link  from "next/link";
+import Link from "next/link";
 import LogoDark from "../../../assets/images/logos/prl.jpg";
 
 const LogoIcon = () => {
   return (
     <Link href="/">
-      <img className="h-5 rounded-xl" src="../../../assets/images/logos/prl.jpg" alt={"logi"} />
+      <img
+        className="h-5 rounded-xl"
+        src="https://download.logo.wine/logo/YouTube/YouTube-Logo.wine.png"
+        alt={"logi"}
+      />
     </Link>
   );
 };
