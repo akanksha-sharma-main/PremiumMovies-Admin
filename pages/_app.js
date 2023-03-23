c282a32a28e60cfc6f78776e28491739f4d5e4b9
Aditya Sharma <akanksha.sharma.main@gmail.com>
2023-03-23 08:35:23 +0000

    Update 3 files

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import {useEffect ,useState} from "react"
-import { useRouter } from 'next/router'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
@@ -10,33 +10,176 @@ import theme from "../src/theme/theme";
 import createEmotionCache from "../src/createEmotionCache";
 import FullLayout from "../src/layouts/FullLayout";
 import "../styles/style.css";
-import LoadingBar from 'react-top-loading-bar'
+import LoadingBar from "react-top-loading-bar";
+import { Fragment, useRef } from "react";
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
-  const [progress, setProgress] = useState(0)
-  const router = useRouter()
+  const [open, setOpen] = useState("");
+  const cancelButtonRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   useEffect(() => {
-    router.events.on('routeChangeStart', () => {setProgress(40)});
-    router.events.on('routeChangeComplete', () => {setProgress(100)});
-  }, [])
+    router.events.on("routeChangeStart", () => {
+      setProgress(40);
+    });
+    router.events.on("routeChangeComplete", () => {
+      setProgress(100);
+    });
+  }, []);
 
   return (
     <CacheProvider value={emotionCache}>
       <LoadingBar
-        color='#03c9d7'
+        color="#03c9d7"
         progress={progress}
         onLoaderFinished={() => setProgress(0)}
       />
+      <div className={open} as={Fragment}>
+        <div className="relative z-10">
+          <div>
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          </div>
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <UserCircleIcon
+                          className="h-6 w-6 text-blue-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 className="text-base font-semibold leading-6 text-gray-900">
+                          Login to Proceed
+                        </h3>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            Welcome back! In order to proceed, please enter your
+                            registered username and password.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3  sm:px-6">
+                    <form className="mt-8 space-y-6" action="#" method="POST">
+                      <input
+                        type="hidden"
+                        name="remember"
+                        defaultValue="true"
+                      />
+                      <div className="-space-y-px rounded-md shadow-sm">
+                        <div>
+                          <label htmlFor="email-address" className="sr-only">
+                            Email address
+                          </label>
+                          <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Email address"
+                          />
+                        </div>
+                        <div>
+                          <label htmlFor="password" className="sr-only">
+                            Password
+                          </label>
+                          <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Password"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                          <input
+                            id="remember-me"
+                            name="remember-me"
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          />
+                          <label
+                            htmlFor="remember-me"
+                            className="ml-2 block text-sm text-gray-900"
+                          >
+                            Remember me
+                          </label>
+                        </div>
+
+                        <div className="text-sm">
+                          <a
+                            href="#"
+                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                          >
+                            Forgot your password?
+                          </a>
+                        </div>
+                      </div>
+
+                      <div>
+                        <button
+                          type="submit"
+                          className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <LockClosedIcon
+                              className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                              aria-hidden="true"
+                            />
+                          </span>
+                          Sign in
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Head>
         <title>Prmovies Admin Panel</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+
       <ThemeProvider theme={theme}>
         <CssBaseline />
+
         <FullLayout>
+          <div
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            click
+          </div>
           <Component {...pageProps} />
         </FullLayout>
       </ThemeProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { Grid } from "@mui/material";
-import {useState} from "react"
+import { useState } from "react";
 import SalesOverview from "../src/components/dashboard/SalseOverview";
 import BaseCard from "../src/components/baseCard/BaseCard";
 import Movie from "../models/Movies";
-import mongoose from 'mongoose';
-import { Card, CardContent,  Button  } from "@mui/material";
+import mongoose from "mongoose";
+import { Card, CardContent, Button } from "@mui/material";
 import {
   Typography,
   Box,
@@ -16,14 +16,16 @@ import {
   Chip,
 } from "@mui/material";
 
-export default function Index({products}) {
-  const [showFullDesc, setShowDesc] = useState(Array(products.length).fill(false))
+export default function Index({ products }) {
+  const [showFullDesc, setShowDesc] = useState(
+    Array(products.length).fill(false)
+  );
 
-  const handleShowMoreClick = (index) =>{
-    const updatedShowDesc = [...showFullDesc]
-    updatedShowDesc[index] = !updatedShowDesc[index]
-    setShowDesc(updatedShowDesc)
-  }
+  const handleShowMoreClick = (index) => {
+    const updatedShowDesc = [...showFullDesc];
+    updatedShowDesc[index] = !updatedShowDesc[index];
+    setShowDesc(updatedShowDesc);
+  };
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} lg={12}>
@@ -34,173 +36,191 @@ export default function Index({products}) {
         <DailyActivity />
       </Grid> */}
       <Grid item xs={12} lg={12}>
-      <BaseCard className="snap-x" title="Product Perfomance">
-      <Table
-        aria-label="simple table"
-        sx={{
-          mt: 3,
-          whiteSpace: "nowrap",
-        }}
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography color="textSecondary" variant="h6">
-                Id
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography color="textSecondary" variant="h6">
-                Title
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography color="textSecondary" variant="h6">
-                Image
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography color="textSecondary" variant="h6">
-                Year
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product, index) => (
-            <TableRow key={product._id}>
-              <TableCell>
-                <Typography
-                  sx={{
-                    fontSize: "15px",
-                    fontWeight: "500",
-                  }}
-                >
-                  {index+1}
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Box>
-                    <Typography
-                      variant="h6"
-                      sx={{
-                        fontWeight: "600",
-                      }}
-                    >
-                      {product.title}
-                    </Typography>
-                    <Typography
-                      color="textSecondary"
-                      sx={{
-                        fontSize: "13px",
-                      }}
-                    >
-                      {product.cast}
-                    </Typography>
-                  </Box>
-                </Box>
-              </TableCell>
-              <TableCell>
-                <Typography color="textSecondary" className="h-10" variant="h6">
-                  <img className="rounded-md h-10" src={product.imageUrl}></img>
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Chip
-                  sx={{
-                    pl: "4px",
-                    pr: "4px",
-                    backgroundColor: "primary.main",
-                    color: "#fff",
-                  }}
-                  size="small"
-                  label={product.releaseYear}
-                ></Chip>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </BaseCard>
-      </Grid>
-      <Grid item xs={12} lg={12}>
-      <Grid container>
-      {products.map((blog, index) => (
-        <Grid
-          key={index}
-          item
-          xs={12}
-          lg={4}
-          sx={{
-            display: "flex",
-            alignItems: "stretch",
-          }}
-        >
-          <Card
+        <BaseCard className="snap-x" title="Product Perfomance">
+          <Table
+            aria-label="simple table"
             sx={{
-              p: 0,
-              width: "100%",
+              mt: 3,
+              whiteSpace: "nowrap",
             }}
           >
-            <img className="w-full" src={blog.imageUrl} alt="img" />
-            <CardContent
-              sx={{
-                paddingLeft: "30px",
-                paddingRight: "30px",
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: "h4.fontSize",
-                  fontWeight: "500",
-                }}
-              >
-                {blog.title}
-              </Typography>
-              <Typography
-                      color="textSecondary"
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography color="textSecondary" variant="h6">
+                    Id
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography color="textSecondary" variant="h6">
+                    Title
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography color="textSecondary" variant="h6">
+                    Image
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography color="textSecondary" variant="h6">
+                    Year
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product, index) => (
+                <TableRow key={product._id}>
+                  <TableCell>
+                    <Typography
                       sx={{
-                        fontSize: "14px",
-                        fontWeight: "400",
-                        mt: 1,
+                        fontSize: "15px",
+                        fontWeight: "500",
                       }}
                     >
-                      {showFullDesc[index] ? blog.desc :blog.desc.substring(0, 280)}<button className="ml-0.5 font-bold text-gray-800" onClick={()=>{handleShowMoreClick(index)}}
-                    >{showFullDesc[index] ? `${" "}Show less...` : `${" "}Show more...`}
-                    </button>
+                      {index + 1}
                     </Typography>
-              <Button
-                variant="contained"
+                  </TableCell>
+                  <TableCell>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Box>
+                        <Typography
+                          variant="h6"
+                          sx={{
+                            fontWeight: "600",
+                          }}
+                        >
+                          {product.title}
+                        </Typography>
+                        <Typography
+                          color="textSecondary"
+                          sx={{
+                            fontSize: "13px",
+                          }}
+                        >
+                          {product.cast}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      color="textSecondary"
+                      className="h-10"
+                      variant="h6"
+                    >
+                      <img
+                        className="rounded-md h-10"
+                        src={product.imageUrl}
+                      ></img>
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      sx={{
+                        pl: "4px",
+                        pr: "4px",
+                        backgroundColor: "primary.main",
+                        color: "#fff",
+                      }}
+                      size="small"
+                      label={product.releaseYear}
+                    ></Chip>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </BaseCard>
+      </Grid>
+      <Grid item xs={12} lg={12}>
+        <Grid container>
+          {products.map((blog, index) => (
+            <Grid
+              key={index}
+              item
+              xs={12}
+              lg={4}
+              sx={{
+                display: "flex",
+                alignItems: "stretch",
+              }}
+            >
+              <Card
                 sx={{
-                  mt: "15px",
+                  p: 0,
+                  width: "100%",
                 }}
-                color={blog.btncolor}
               >
-                View
-              </Button>
-            </CardContent>
-          </Card>
+                <img className="w-full" src={blog.imageUrl} alt="img" />
+                <CardContent
+                  sx={{
+                    paddingLeft: "30px",
+                    paddingRight: "30px",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontSize: "h4.fontSize",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {blog.title}
+                  </Typography>
+                  <Typography
+                    color="textSecondary"
+                    sx={{
+                      fontSize: "14px",
+                      fontWeight: "400",
+                      mt: 1,
+                    }}
+                  >
+                    {showFullDesc[index]
+                      ? blog.desc
+                      : blog.desc.substring(0, 280)}
+                    <button
+                      className="ml-0.5 font-bold text-gray-800"
+                      onClick={() => {
+                        handleShowMoreClick(index);
+                      }}
+                    >
+                      {showFullDesc[index]
+                        ? `${" "}Show less...`
+                        : `${" "}Show more...`}
+                    </button>
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      mt: "15px",
+                    }}
+                    color={blog.btncolor}
+                  >
+                    View
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
       </Grid>
     </Grid>
   );
 }
 
-
 export async function getServerSideProps(context) {
   if (!mongoose.connections[0].readyState) {
-    await mongoose.connect('mongodb+srv://adityastudio:PassworD@cluster0.kllpeia.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect(
+      "mongodb+srv://adityastudio:PassworD@cluster0.kllpeia.mongodb.net/?retryWrites=true&w=majority"
+    );
   }
-  let movie = await Movie.find()
+  let movie = await Movie.find();
   return {
     props: { products: JSON.parse(JSON.stringify(movie)) },
-  }
+  };
 }
